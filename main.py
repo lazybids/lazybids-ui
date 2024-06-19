92e247f990c5d5bd4a943451d29b5e50f9b2d350
Roelant Eijgelaar <roelanteijgelaar@hotmail.com>
2024-06-19 18:57:19 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import html 
+from typing import Optional
+from fastapi import FastAPI, Request, Header, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import lazybids
+
+
+from typing import List
+
+import asyncio
+
+import src.models as models
+import datetime
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+import hashlib
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+engine = create_engine("sqlite:///database.db")
+templates = Jinja2Templates(directory="templates")
+
+
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+
+    context = {"request": request,}
+    return templates.TemplateResponse("root.html", context )
+
+
+@app.get("/datasets", response_class=HTMLResponse)
+async def root(request: Request):
+    with Session(engine) as session:
+        statement = select(models.Dataset)
+        datasets = session.exec(statement).all()
+    print(datasets)
+    context = {"request": request,'datasets':datasets}
+    return templates.TemplateResponse("components/datasets.html", context )
+
+
+@app.get("/dataset/{ds_id}", response_class=HTMLResponse)
+async def dataset(request: Request, ds_id:int):
+    with Session(engine) as session:
+        statement = select(models.Dataset).where(models.Dataset.id==ds_id)
+        dataset = session.exec(statement).first()
+    ds = lazybids.Dataset.from_folder(dataset.folder, load_scans_in_memory=False)
+
+# @app.get("/search", response_class=HTMLResponse)
+# async def root(request: Request, search_text: str):
+
+#     # Initialize beanie with the Product document class
+#     my_search = search_text
+#     res = search_youtube('Best '+str(my_search)+' 2024')
+#     res2 = search_youtube('Best '+str(my_search)+' 2023')
+    
+#     yts = []
+#     for i in res['items']+res2['items']:
+#         if i['id']['kind'] == 'youtube#video':
+#             yts.append(models.YTVideo(**{'id': i['id']['videoId'],
+#                                 'title': html.unescape(i['snippet']['title']),
+#                             'channel':i['snippet']['channelTitle'],'my_search':my_search}))
+#     context = {"request": request, 'yts': yts}
+#     return templates.TemplateResponse("components/yt_carousel.html", context )
+
+# @app.get("/yt_products/{yt_id}/{my_search}", response_class=HTMLResponse)
+# async def yt_products(request: Request, yt_id:str, my_search:str='Cordless vacuum'):
+#     try:
+#         transcript = YouTubeTranscriptApi.get_transcript( yt_id)
+#     except:
+#         return templates.TemplateResponse("components/products_empty.html", {"request": request} )
+#     task = get_productList_from_transcript.delay(transcript, my_search)  
+#     print(f'Spawned process for {task.id}')
+#     return templates.TemplateResponse("components/products_delayed.html", {"request": request, 'mytask':{"id":str(task.id)},'delay':500})
+
+# @app.get("/yt_products_status/{task_id}", response_class=HTMLResponse)
+# async def yt_products_status(request: Request, task_id:str):
+#     print(f'got status request for {task_id}')
+#     task = get_productList_from_transcript.AsyncResult(task_id)
+#     if task.state == 'FAILURE':
+#         return templates.TemplateResponse("components/products_empty.html", {"request": request} )
+#     elif task.state == 'PENDING':
+        
+#         return templates.TemplateResponse("components/products_delayed.html", {"request": request, 'mytask':{"id":str(task_id)},'delay':2500} )
+#     elif task.state == 'SUCCESS':
+#         if not(task.result):
+#             return templates.TemplateResponse("components/products_empty.html", {"request": request} )
+#         products = models.ProductsList(products=task.result[1]['products'], transcript=str(task.result[0]))
+#         context = {"request": request, 'products': products}
+#         return templates.TemplateResponse("components/products.html", context )
+#     else:
+#         return templates.TemplateResponse("components/products_delayed.html", {"request": request,'mytask':{"id":str(task_id)},'delay':2500}  )
---
+++ src/models.py
@@ -0,0 +1,57 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import Optional, List
+
+from sqlmodel import Field, Session, SQLModel, create_engine
+from pathlib import Path
+
+class Dataset(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    folder: str
+
+engine = create_engine("sqlite:///database.db")
+
+
+SQLModel.metadata.create_all(engine)
+
+
+# Now, we can use the response_model parameter using only a base model
+# rather than having to use the OpenAISchema class
+class Currency(str, Enum):
+    dollar = '$'
+    euro = '€'
+
+class Argument(BaseModel):
+    argument: str
+    start_time: float
+
+class Product(BaseModel):
+    name: str
+    name_wo_brand: str
+    brand: str
+    price: int
+    currency: Currency
+    pros: List[str]
+    cons: List[str]
+
+class ProductList(BaseModel):
+    products: List[Product]
+
+class ProductsList(BaseModel):
+    products: List[Product]
+    transcript: str
+    #transcript_hash: str#Indexed(str, unique=True) # type: ignore
+
+
+class YTVideo(BaseModel):
+    id: str
+    title: str
+    channel: str
+    my_search: str
+
+class Task(BaseModel):
+    id: str
+
+class ValuableTranscript(BaseModel):
+    valuable: bool
